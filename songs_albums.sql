c97bc8b5b80612fc2c29fc25e3eea35537a189a7
Cptn Reef <sharif.staffordbz@gmail.com>
2021-04-08 21:36:50 -0400

    hmw1-2

--- songs_albums.sql
+++ songs_albums.sql
@@ -42,16 +42,42 @@ SELECT * FROM Albums;
 /* 
  * TODO: Write a table join query to construct a table of Song Name : Album Name
  */
+.print 'CHALLENGE 1'
+.print ''
+SELECT Songs.name FROM Songs
+JOIN Albums ON album_id = Albums.id;
+
+.print ''
 
 /*
  * TODO: Find all albums published between 1970 and 1980.
  */
+.print 'CHALLENGE 2'
+.print ''
+SELECT Albums.name FROM Albums
+WHERE year_published > 1970 AND year_published <= 1980;
+.print ''
 
 /*
  * TODO: Find all songs on albums published between 1970 and 1980. 
  *(Hint: Use a table join.)
  */
- 
+ .print 'CHALLENGE 3'
+.print ''
+SELECT Songs.name FROM Songs
+JOIN Albums ON album_id = Albums.id
+WHERE year_published <= 2004 AND year_published >= 2002;
+
+.print ''
+
+
 /*
  * TODO: Find all songs on albums with names containing 'California'.
  */
+
+.print 'CHALLENGE 4'
+.print ''
+SELECT Songs.name FROM Songs
+JOIN Albums ON album_id = Albums.id
+WHERE Songs.name LIKE '%California%';
+.print ''
